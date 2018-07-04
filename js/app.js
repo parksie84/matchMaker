fa19b2afb523a2b24e14809c0c637f206ccc727f
parksie84 <40614212+parksie84@users.noreply.github.com>
2018-07-03 23:35:00 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,38 +1,194 @@
-/*
- * Create a list that holds all of your cards
- */
+/// Create a list that holds all of your cards
+const allCards = [
+  "fa fa-diamond",
+  "fa fa-diamond",
+  "fa fa-paper-plane-o",
+  "fa fa-paper-plane-o",
+  "fa fa-anchor",
+  "fa fa-anchor",
+  "fa fa-bolt",
+  "fa fa-bolt",
+  "fa fa-cube",
+  "fa fa-cube",
+  "fa fa-leaf",
+  "fa fa-leaf",
+  "fa fa-bicycle",
+  "fa fa-bicycle",
+  "fa fa-bomb",
+  "fa fa-bomb"
+];
 
+// Declaring variables 
+const cardDeck = document.querySelector(".deck");
+const stars = document.querySelectorAll(".fa-star");
+const ratingContainer = document.querySelector(".stars");
+let openedCards = [];
+let matchedCards = [];
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
-// Shuffle function from http://stackoverflow.com/a/2450976
+/// Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+/// Function to start new game play
+function startGame() {
+  
+  // Shuffle deck of cards
+  cards = shuffle(allCards);
+  
+  for (let i = 0; i < allCards.length; i++) {
+    const card = document.createElement("li");
+    card.classList.add("card");
+    card.innerHTML = `<i class= "${allCards[i]}"> </i>`;
+    cardDeck.appendChild(card);
+
+    // Add click event to each card
+    click(card);
+  }
+}
+
+/// Click function 
+function click(card) {
+  
+  // Click event listener
+  card.addEventListener("click", function() {
+    const currentCard = this;
+    const previousCard = openedCards[0];
+    
+    // Click to open card
+    if (openedCards.length === 1) {
+      card.classList.add("open", "show", "disable");
+      openedCards.push(this);
+      
+      // Compare two opened cards
+      compare(currentCard, previousCard);
+    } else {
+      
+      // Unopened cards
+      currentCard.classList.add("open", "show", "disable");
+      openedCards.push(this);
     }
+  });
+}
 
-    return array;
+/// Compare two cards for a match
+function compare(currentCard, previousCard) {
+  
+  // Matched cards
+  if (currentCard.innerHTML === previousCard.innerHTML) {
+    currentCard.classList.add("match");
+    previousCard.classList.add("match");
+    matchedCards.push(currentCard, previousCard);
+    openedCards = [];
+    
+    // Checks if the game is over
+    isOver();
+  } else {
+    
+    // Turn card to blank in set time
+    setTimeout(function() {
+      currentCard.classList.remove("open", "show");
+      previousCard.classList.remove("open", "show");
+      openedCards = [];
+    }, 250);
+  }
+
+  // Add a move to the counter
+  addMove();
+}
+
+// End game
+function isOver() {
+  if (matchedCards.length === allCards.length) {
+    clearInterval(interval);
+    alert("Congratulations! You have matched all the cards.");
+  }
+}
+
+/// Game timer
+var second = 0, minute = 0;
+var timer = document.querySelector(".timer");
+var interval;
+function startTimer(){
+    interval = setInterval(function(){
+        timer.innerHTML = minute+" mins - "+second+" secs";
+        second++;
+        if(second == 60){
+            minute++;
+            second = 0;
+        }
+    },1000);
+}  
+
+
+/// Move counter
+const movesContainer = document.querySelector(".moves");
+let moves = 0;
+movesContainer.innerHTML = 0;
+function addMove() {
+  moves++;
+  movesContainer.innerHTML = moves;
+  
+  // Start timer after first move
+  if (moves === 1){
+    startTimer();
+  }
+    
+  // Rating based on amount of moves
+  if (moves > 14 && moves < 18){
+    for( i= 0; i < 3; i++){
+      if(i > 1){
+        stars[i].style.visibility = "collapse";
+            }
+        }
+    }
+  else if (moves > 22){
+    for( i= 0; i < 3; i++){
+      if(i > 0){
+        stars[i].style.visibility = "collapse";
+            }
+        }
+    }
 }
 
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+/// Setup restart button
+const restartButton = document.querySelector(".restart");
+restartButton.addEventListener("click", function() {
+  
+  // Turning cards to blank
+  cardDeck.innerHTML = "";
+  
+  // Reset the game
+  startGame();
+  
+  // Reset any variables
+  matchedCards = [];
+  moves = 0;
+  movesContainer.innerHTML = moves;
+  ratingContainer.innerHTML = '<li><i class="fa fa-star"></i></li> <li><i class="fa fa-star"></i></li> <li><i class="fa fa-star"></i></li>';
+  
+  //Reset the timer
+  var timer = document.querySelector(".timer");
+  second = 0;
+  minute = 0; 
+  timer.innerHTML = "0 mins - 0 secs";
+  clearInterval(interval);
+  
+});
+
+/// Replay the game
+startGame();
+
